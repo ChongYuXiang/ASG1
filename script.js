fd71fc92b76aa21a1209346bbc9d1eef5d7f992b
ChongYuXiang <s10257151@connect.np.edu.sg>
2023-12-01 21:41:52 +0800

    Final Update

---
+++ script.js
@@ -0,0 +1,19 @@
+function navFunction1() {
+    const element = document.getElementById("profile");
+    element.scrollIntoView();
+}
+function navFunction2() {
+    const element = document.getElementById("events");
+    element.scrollIntoView();
+}
+function navFunction3() {
+    const element = document.getElementById("songs");
+    element.scrollIntoView();
+}
+function navFunction4() {
+    const element = document.getElementById("videos");
+    element.scrollIntoView();
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+}
